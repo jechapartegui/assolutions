935133275bc0ac3adeb1530747ec1d6e5d98a4f8
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-07-31 22:52:54 +0200

    feat(souscription): register tunnel routes and Angular module

--- apps/assolutions-front/src/app/app.module.ts
+++ apps/assolutions-front/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -9,23 +11,14 @@ import { MenuComponent } from './menu/menu.component';
 import { AdherentComponent } from './adherent/main/adherent.component';
 import { CoursComponent } from './cours/cours.component';
 import { SeanceComponent } from './seance/seance.component';
-import { StaticClass } from './global';
 import { NotifJechaComponent } from './custom-notification/custom-notification.component';
-import { HttpClientModule } from '@angular/common/http';
 import { CustomButtonComponent } from './custom-button/custom-button.component';
-import { ErrorService } from '../services/error.service';
 import { MultifiltersMenuPipe } from '../filters/multifilters-menu.pipe';
-import { GlobalService } from '../services/global.services';
 import { DureeHHMMPipe } from '../pipe/duree.pipe';
 import { SeanceEditorComponent } from './seance-editor/seance-editor.component';
 import { SeanceListComponent } from './seance-list/seance-list.component';
-import { RefDataStore } from '../store/ref-data.store';
-import { MenuStore } from '../store/menu.store';
-import { AppStore } from './app.store';
 import { CoursEditorComponent } from './cours-editor/cours-editor.component';
-import { SeanceStore } from '../store/seance.store';
 import { CoursListComponent } from './cours-list/cours-list.component';
-import { CoursStore } from '../store/cours.store';
 import { AdherentListComponent } from './adherent/list/adherent-list.component';
 import { AdherentEditorComponent } from './adherent/detail/adherent-editor.component';
 import { MailTemplatePreviewComponent } from './mail-template-preview/mail-template-preview.component';
@@ -47,25 +40,74 @@ import { OperationsComponent } from './operations/operations.component';
 import { LieuSelectorComponent } from './component/lieu-selector/lieu-selector.component';
 import { CreerCompteComponent } from './creer-compte/creer-compte.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { SouscriptionTunnelComponent } from './souscription/souscription-tunnel.component';
+
+import { StaticClass } from './global';
+import { ErrorService } from '../services/error.service';
+import { GlobalService } from '../services/global.services';
+import { RefDataStore } from '../store/ref-data.store';
+import { MenuStore } from '../store/menu.store';
+import { AppStore } from './app.store';
+import { SeanceStore } from '../store/seance.store';
+import { CoursStore } from '../store/cours.store';
+
 @NgModule({
-  declarations: [ CreerCompteComponent, InscriptionComponent,
-    AppComponent, PersonneSelectorComponent, ContratProfComponent, ProfesseurComponent, EnvoiMailComponent, ProjetMailComponent, ComptabiliteComponent, OperationsComponent, LieuSelectorComponent,
-    AppComponent, LoginComponent, MenuComponent,MultifiltersMenuPipe, CustomButtonComponent, SeanceComponent, SeanceEditorComponent,  LieuComponent, SaisonComponent,
-    SeanceListComponent, CoursComponent, CoursEditorComponent, CoursListComponent, MailTemplatePreviewComponent, MaSeanceComponent, DashboardComponent, CompteBancaireComponent, MenuAdminComponent,
-    NotifJechaComponent, DureeHHMMPipe, AdherentComponent, AdherentListComponent, AdherentEditorComponent, GroupeComponent, AddInfoEditorComponent, MenuAdminComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    CreerCompteComponent,
+    PersonneSelectorComponent,
+    LieuSelectorComponent,
+    ContratProfComponent,
+    ProfesseurComponent,
+    EnvoiMailComponent,
+    ProjetMailComponent,
+    ComptabiliteComponent,
+    OperationsComponent,
+    MultifiltersMenuPipe,
+    CustomButtonComponent,
+    SeanceComponent,
+    SeanceEditorComponent,
+    SeanceListComponent,
+    LieuComponent,
+    SaisonComponent,
+    CoursComponent,
+    CoursEditorComponent,
+    CoursListComponent,
+    MailTemplatePreviewComponent,
+    MaSeanceComponent,
+    DashboardComponent,
+    CompteBancaireComponent,
+    MenuAdminComponent,
+    NotifJechaComponent,
+    DureeHHMMPipe,
+    AdherentComponent,
+    AdherentListComponent,
+    AdherentEditorComponent,
+    GroupeComponent,
+    AddInfoEditorComponent,
+    InscriptionComponent,
+    SouscriptionTunnelComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,   // pour formGroup/formControlName
+    ReactiveFormsModule,
     AppRoutingModule,
-    // Ajoutez ici l'un des modules d'animations selon vos besoins
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
-  providers: [ StaticClass,
+  providers: [
+    StaticClass,
     ErrorService,
     AppStore,
-MenuStore, RefDataStore,GlobalService, SeanceStore, CoursStore],
-  bootstrap: [AppComponent]
+    MenuStore,
+    RefDataStore,
+    GlobalService,
+    SeanceStore,
+    CoursStore,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
