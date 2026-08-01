ffc9909ceda65601d34c75646f4d8e10a831293a
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-01 07:40:27 +0200

    fix(souscription): remove configured default group

--- apps/assolutions-back/src/groupes/groupes.entity.ts
+++ apps/assolutions-back/src/groupes/groupes.entity.ts
@@ -23,9 +23,6 @@ export class GroupesEntity {
   @Column({ type: 'boolean', nullable: true })
   visible: boolean | null;
 
-  @Column({ type: 'boolean', default: false })
-  par_defaut: boolean;
-
   @Column({ type: 'int', nullable: true })
   age_min: number | null;
 
