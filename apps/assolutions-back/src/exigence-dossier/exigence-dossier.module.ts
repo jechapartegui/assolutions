05f308ada5e3879acc0edc000722ea69a526886a
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-02 08:33:35 +0200

    feat(back): register dossier evaluation data sources

--- apps/assolutions-back/src/exigence-dossier/exigence-dossier.module.ts
+++ apps/assolutions-back/src/exigence-dossier/exigence-dossier.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Contact } from '../contact/contact.entity';
+import { DocumentEntity } from '../document/document.entity';
 import { GroupesEntity } from '../groupes/groupes.entity';
+import { PersonneEntity } from '../personne/personne.entity';
 import { SaisonEntity } from '../saison/saison.entity';
 import { TarifInscriptionEntity } from '../tarif_inscription/tarif_inscription.entity';
+import { DossierPersonneController } from './dossier-personne.controller';
 import { ExigenceDossierController } from './exigence-dossier.controller';
 import { ExigenceDossierEntity } from './exigence-dossier.entity';
 import { ExigenceDossierPorteeEntity } from './exigence-dossier-portee.entity';
@@ -16,12 +20,15 @@ import { ExigenceDossierService } from './exigence-dossier.service';
       ExigenceDossierEntity,
       ExigenceDossierPorteeEntity,
       ReponseExigenceDossierEntity,
+      PersonneEntity,
+      Contact,
+      DocumentEntity,
       SaisonEntity,
       GroupesEntity,
       TarifInscriptionEntity,
     ]),
   ],
-  controllers: [ExigenceDossierController],
+  controllers: [ExigenceDossierController, DossierPersonneController],
   providers: [ExigenceDossierService],
   exports: [ExigenceDossierService, TypeOrmModule],
 })
