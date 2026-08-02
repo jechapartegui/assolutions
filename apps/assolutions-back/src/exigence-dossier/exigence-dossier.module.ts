a581c2d33b90fbaabc7341217294a63a418977a7
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-02 14:52:35 +0200

    feat(back): register dossier orchestration service

--- apps/assolutions-back/src/exigence-dossier/exigence-dossier.module.ts
+++ apps/assolutions-back/src/exigence-dossier/exigence-dossier.module.ts
@@ -4,10 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Contact } from '../contact/contact.entity';
 import { DocumentEntity } from '../document/document.entity';
 import { GroupesEntity } from '../groupes/groupes.entity';
+import { InscriptionSaisonEntity } from '../inscription_saison/inscription_saison.entity';
+import { LienGroupeEntity } from '../lien_groupe/lien_groupe.entity';
 import { PersonneEntity } from '../personne/personne.entity';
 import { SaisonEntity } from '../saison/saison.entity';
+import { SouscriptionEntity } from '../souscription/souscription.entity';
+import { SouscriptionPersonneEntity } from '../souscription/souscription-personne.entity';
+import { SouscriptionPersonneGroupeEntity } from '../souscription/souscription-personne-groupe.entity';
 import { TarifInscriptionEntity } from '../tarif_inscription/tarif_inscription.entity';
 import { DossierPersonneController } from './dossier-personne.controller';
+import { DossierPersonneSaisonEntity } from './dossier-personne-saison.entity';
 import { ExigenceDossierController } from './exigence-dossier.controller';
 import { ExigenceDossierEntity } from './exigence-dossier.entity';
 import { ExigenceDossierPorteeEntity } from './exigence-dossier-portee.entity';
@@ -16,6 +22,7 @@ import { ExigenceDossierService } from './exigence-dossier.service';
 import { PreuveMedicaleController } from './preuve-medicale.controller';
 import { PreuveMedicaleEntity } from './preuve-medicale.entity';
 import { PreuveMedicaleService } from './preuve-medicale.service';
+import { SouscriptionDossierService } from './souscription-dossier.service';
 
 @Module({
   imports: [
@@ -24,12 +31,18 @@ import { PreuveMedicaleService } from './preuve-medicale.service';
       ExigenceDossierPorteeEntity,
       ReponseExigenceDossierEntity,
       PreuveMedicaleEntity,
+      DossierPersonneSaisonEntity,
       PersonneEntity,
       Contact,
       DocumentEntity,
       SaisonEntity,
       GroupesEntity,
       TarifInscriptionEntity,
+      SouscriptionEntity,
+      SouscriptionPersonneEntity,
+      SouscriptionPersonneGroupeEntity,
+      InscriptionSaisonEntity,
+      LienGroupeEntity,
     ]),
   ],
   controllers: [
@@ -37,10 +50,15 @@ import { PreuveMedicaleService } from './preuve-medicale.service';
     DossierPersonneController,
     PreuveMedicaleController,
   ],
-  providers: [ExigenceDossierService, PreuveMedicaleService],
+  providers: [
+    ExigenceDossierService,
+    PreuveMedicaleService,
+    SouscriptionDossierService,
+  ],
   exports: [
     ExigenceDossierService,
     PreuveMedicaleService,
+    SouscriptionDossierService,
     TypeOrmModule,
   ],
 })
