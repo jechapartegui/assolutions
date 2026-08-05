751b6da101e771d1bd29289e421d4ade9b41e1df
J-E <jechapartegui@gmail.com>
2026-08-05 18:25:27 +0200

    avant PR16 ?

--- apps/debug-front/src/main.ts
+++
@@ -1,5 +0,0 @@
-import '@angular/localize/init';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
