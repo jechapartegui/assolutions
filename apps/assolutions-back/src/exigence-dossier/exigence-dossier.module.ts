e9f046edd3dc3611742d715d65c77d21657f0645
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-05 14:48:39 +0200

    fix(souscription): export medical checkout guard

--- apps/assolutions-back/src/exigence-dossier/exigence-dossier.module.ts
+++ apps/assolutions-back/src/exigence-dossier/exigence-dossier.module.ts
@@ -29,6 +29,7 @@ import { PreuveMedicaleService } from './preuve-medicale.service';
 import { SouscriptionAdminService } from './souscription-admin.service';
 import { SouscriptionContextEnricherService } from './souscription-context-enricher.service';
 import { SouscriptionDossierService } from './souscription-dossier.service';
+import { SouscriptionMedicalGuardService } from './souscription-medical-guard.service';
 import { SouscriptionNotificationService } from './souscription-notification.service';
 import { SouscriptionViewEnricherService } from './souscription-view-enricher.service';
 
@@ -56,7 +57,11 @@ import { SouscriptionViewEnricherService } from './souscription-view-enricher.se
       LienGroupeEntity,
     ]),
   ],
-  controllers: [ExigenceDossierController, DossierPersonneController, PreuveMedicaleController],
+  controllers: [
+    ExigenceDossierController,
+    DossierPersonneController,
+    PreuveMedicaleController,
+  ],
   providers: [
     DossierDocumentService,
     ExigenceDossierService,
@@ -64,6 +69,7 @@ import { SouscriptionViewEnricherService } from './souscription-view-enricher.se
     SouscriptionAdminService,
     SouscriptionContextEnricherService,
     SouscriptionDossierService,
+    SouscriptionMedicalGuardService,
     SouscriptionViewEnricherService,
     SouscriptionNotificationService,
   ],
@@ -74,6 +80,7 @@ import { SouscriptionViewEnricherService } from './souscription-view-enricher.se
     SouscriptionAdminService,
     SouscriptionContextEnricherService,
     SouscriptionDossierService,
+    SouscriptionMedicalGuardService,
     SouscriptionViewEnricherService,
     SouscriptionNotificationService,
     TypeOrmModule,
