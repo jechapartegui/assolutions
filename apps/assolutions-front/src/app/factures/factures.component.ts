87ce9ca35dba147e70c1c9662992f426b9e1e761
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-08 00:05:11 +0300

    chore(front): clean unused screens and fix config

--- apps/assolutions-front/src/app/factures/factures.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: false,
-  selector: 'app-factures',
-  templateUrl: './factures.component.html',
-  styleUrls: ['./factures.component.css']
-})
-export class FacturesComponent {}
--- apps/assolutions-front/src/app/suivi-mail/suivi-mail.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: false,
-  selector: 'app-suivi-mail',
-  templateUrl: './suivi-mail.component.html',
-  styleUrls: ['./suivi-mail.component.css']
-})
-export class SuiviMailComponent {}
