2041c75de9f417e10e8d9fb27ca7cc99ba67dbac
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-10 19:41:29 +0300

    feat(ffrs): register export service

--- apps/assolutions-back/src/personne/personne.module.ts
+++ apps/assolutions-back/src/personne/personne.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AccessControlModule } from '../common/access-control.module';
+import { FfrsExportService } from './ffrs-export.service';
 import { PersonneController } from './personne.controller';
 import { PersonneEntity } from './personne.entity';
 import { PersonneService } from './personne.service';
 
-import { AccessControlModule } from '../common/access-control.module'; // ✅
 @Module({
   imports: [TypeOrmModule.forFeature([PersonneEntity]), AccessControlModule],
   controllers: [PersonneController],
-  providers: [PersonneService],
+  providers: [PersonneService, FfrsExportService],
 })
 export class PersonneModule {}
