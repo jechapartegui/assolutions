940cb8b9c3a77faea55cd9a0b81e94fe63223d07
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-20 01:24:17 +0300

    security: remove public HelloAsso test endpoint

--- apps/assolutions-back/src/helloasso/helloasso.module.ts
+++ apps/assolutions-back/src/helloasso/helloasso.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { HelloAssoConfigGuard } from './helloasso-config.guard';
-import { HelloAssoController } from './helloasso.controller';
 import { HelloAssoService } from './helloasso.service';
 
 @Module({
-  controllers: [HelloAssoController],
   providers: [HelloAssoService, HelloAssoConfigGuard],
-  exports: [HelloAssoService],
+  exports: [HelloAssoService, HelloAssoConfigGuard],
 })
 export class HelloAssoModule {}
