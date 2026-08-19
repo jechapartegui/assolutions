8df4d1e129923dc5d93fe122b535cc4e5c3ce5e9
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-20 01:00:47 +0300

    security: export project membership guard

--- apps/assolutions-back/src/common/access-control.module.ts
+++ apps/assolutions-back/src/common/access-control.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { AccessControlService } from './access-control.service';
+import { ProjectAccessGuard } from './guards/project-access.guard';
 import { ProjectAdminGuard } from './guards/project-admin.guard';
 
 @Global()
 @Module({
-  providers: [AccessControlService, ProjectAdminGuard],
-  exports: [AccessControlService, ProjectAdminGuard],
+  providers: [AccessControlService, ProjectAccessGuard, ProjectAdminGuard],
+  exports: [AccessControlService, ProjectAccessGuard, ProjectAdminGuard],
 })
 export class AccessControlModule {}
