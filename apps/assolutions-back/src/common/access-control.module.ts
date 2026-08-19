0ce36c94290c7b38edc950504bb2ea6b6a8844a6
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-20 01:01:51 +0300

    security: provide super admin guard centrally

--- apps/assolutions-back/src/common/access-control.module.ts
+++ apps/assolutions-back/src/common/access-control.module.ts
@@ -2,10 +2,21 @@ import { Global, Module } from '@nestjs/common';
 import { AccessControlService } from './access-control.service';
 import { ProjectAccessGuard } from './guards/project-access.guard';
 import { ProjectAdminGuard } from './guards/project-admin.guard';
+import { SuperAdminGuard } from './guards/super-admin.guard';
 
 @Global()
 @Module({
-  providers: [AccessControlService, ProjectAccessGuard, ProjectAdminGuard],
-  exports: [AccessControlService, ProjectAccessGuard, ProjectAdminGuard],
+  providers: [
+    AccessControlService,
+    ProjectAccessGuard,
+    ProjectAdminGuard,
+    SuperAdminGuard,
+  ],
+  exports: [
+    AccessControlService,
+    ProjectAccessGuard,
+    ProjectAdminGuard,
+    SuperAdminGuard,
+  ],
 })
 export class AccessControlModule {}
