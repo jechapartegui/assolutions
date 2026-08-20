25826a7a7497e759b0f31736a2967ab74795673f
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-20 09:10:57 +0300

    security: export centralized access-control service

--- apps/assolutions-back/src/common/access-control.module.ts
+++ apps/assolutions-back/src/common/access-control.module.ts
@@ -1,12 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectEntity } from '../project/project.entity';
+import { AccessControlService } from './access-control.service';
 import { ProjectAdminGuard } from './guards/project-admin.guard';
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([ProjectEntity])],
-  providers: [ProjectAdminGuard],
-  exports: [ProjectAdminGuard],
+  providers: [AccessControlService, ProjectAdminGuard],
+  exports: [AccessControlService, ProjectAdminGuard],
 })
 export class AccessControlModule {}
