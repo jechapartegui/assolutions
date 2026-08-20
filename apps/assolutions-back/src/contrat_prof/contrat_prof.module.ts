cb97f2efa0d7e23a3b0a15494bf40d99a4ebb5dd
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-20 09:36:58 +0300

    security: inject professor repository for contract scoping

--- apps/assolutions-back/src/contrat_prof/contrat_prof.module.ts
+++ apps/assolutions-back/src/contrat_prof/contrat_prof.module.ts
@@ -1,16 +1,23 @@
-﻿import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AccessControlModule } from '../common/access-control.module';
+import { ProfesseurEntity } from '../professeur/professeur.entity';
+import { SaisonEntity } from '../saison/saison.entity';
+import { SaisonService } from '../saison/saison.service';
 import { ContratProfController } from './contrat_prof.controller';
 import { ContratProfEntity } from './contrat_prof.entity';
 import { ContratProfService } from './contrat_prof.service';
-import { SaisonService } from '../saison/saison.service';
-import { SaisonEntity } from '../saison/saison.entity';
-
-import { AccessControlModule } from '../common/access-control.module'; // ✅
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ContratProfEntity, SaisonEntity]), AccessControlModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      ContratProfEntity,
+      SaisonEntity,
+      ProfesseurEntity,
+    ]),
+    AccessControlModule,
+  ],
   controllers: [ContratProfController],
   providers: [ContratProfService, SaisonService],
 })
