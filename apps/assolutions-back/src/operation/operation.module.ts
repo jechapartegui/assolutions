17735dc38fa535fb0c9244139cccceaa2b470f4c
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-23 09:44:52 +0200

    fix: remove redundant flux service from operation module

--- apps/assolutions-back/src/operation/operation.module.ts
+++ apps/assolutions-back/src/operation/operation.module.ts
@@ -1,17 +1,23 @@
-﻿import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AccessControlModule } from '../common/access-control.module';
+import { CompteBancaireEntity } from '../compte_bancaire/compte_bancaire.entity';
+import { FluxFinancierEntity } from '../flux_financier/flux_financier.entity';
 import { OperationController } from './operation.controller';
 import { OperationEntity } from './operation.entity';
 import { OperationService } from './operation.service';
-import { FluxFinancierEntity } from '../flux_financier/flux_financier.entity';
-import { FluxFinancierService } from '../flux_financier/flux_financier.service';
 
-import { AccessControlModule } from '../common/access-control.module'; // ✅
-import { CompteBancaireEntity } from '../compte_bancaire/compte_bancaire.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([OperationEntity, FluxFinancierEntity, CompteBancaireEntity]), AccessControlModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      OperationEntity,
+      FluxFinancierEntity,
+      CompteBancaireEntity,
+    ]),
+    AccessControlModule,
+  ],
   controllers: [OperationController],
-  providers: [OperationService, FluxFinancierService],
+  providers: [OperationService],
 })
 export class OperationModule {}
