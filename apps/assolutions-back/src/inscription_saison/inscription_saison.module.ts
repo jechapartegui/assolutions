975b9452f26f0a5641304506ea76598806a736de
jechapartegui <77270103+jechapartegui@users.noreply.github.com>
2026-08-23 09:44:20 +0200

    fix: remove redundant saison service from inscription saison module

--- apps/assolutions-back/src/inscription_saison/inscription_saison.module.ts
+++ apps/assolutions-back/src/inscription_saison/inscription_saison.module.ts
@@ -1,16 +1,18 @@
-﻿import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AccessControlModule } from '../common/access-control.module';
+import { SaisonEntity } from '../saison/saison.entity';
 import { InscriptionSaisonController } from './inscription_saison.controller';
 import { InscriptionSaisonEntity } from './inscription_saison.entity';
 import { InscriptionSaisonService } from './inscription_saison.service';
-import { SaisonEntity } from '../saison/saison.entity';
-import { SaisonService } from '../saison/saison.service';
 
-import { AccessControlModule } from '../common/access-control.module'; // ✅
 @Module({
-  imports: [TypeOrmModule.forFeature([InscriptionSaisonEntity, SaisonEntity]), AccessControlModule],
+  imports: [
+    TypeOrmModule.forFeature([InscriptionSaisonEntity, SaisonEntity]),
+    AccessControlModule,
+  ],
   controllers: [InscriptionSaisonController],
-  providers: [InscriptionSaisonService, SaisonService],
+  providers: [InscriptionSaisonService],
 })
 export class InscriptionSaisonModule {}
